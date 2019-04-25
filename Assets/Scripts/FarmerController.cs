fcc4c54d303a0ba150f57104e83cbf387f57a78c
linyu <lin.y77@qq.com>
2019-04-25 14:43:38 +0800

    add door interaction

--- Assets/Scripts/FarmerController.cs
+++ Assets/Scripts/FarmerController.cs
@@ -6,10 +6,12 @@ public class FarmerController : MonoBehaviour {
     public float speed = 10.0f;
     private float translation;
     private float straffe;
+    private GameObject door;
 
 	// Use this for initialization
 	void Start () {
         Cursor.lockState = CursorLockMode.Locked;
+        door = GameObject.Find("Door");
 	}
 	
 	// Update is called once per frame
@@ -21,6 +23,19 @@ public class FarmerController : MonoBehaviour {
         if (Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
+        } else if (Input.GetKeyDown("f"))
+        {
+            print("yeah");
+            if (Vector3.Distance(transform.position, door.transform.position) < 2f)
+            {
+                if (door.transform.rotation == Quaternion.Euler(0, 0, 0))
+                {
+                    door.transform.rotation = Quaternion.Euler(0, 90, 0);
+                } else
+                {
+                    door.transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+            }
         }
     }
 }
